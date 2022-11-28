3092f33bab0937025bbe3927b37a2992aa60182c
dhananjai14 <dhananjai.eee@gmail.com>
2022-11-28 11:06:51 +0530

    Update

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+
 from typing import List
 
-requirements_file_name = 'requirements.txt'
-hyphen_e_dot = '-e .'
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
-    with open (requirements_file_name) as requirements:
-        requirements_lst = requirements.readlines()
-    requirements_lst = [requirement_name.replace('\n', '') for requirement_name in requirements_list]
-    if hyphen_e_dot in requirements_lst:
-        requirements_lst.remove(hyphen_e_dot)
-    return requirements_lst
+    
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
 
 setup(
-    name = 'sensor',
-    version = '0.0.1',
-    author = 'Dhananjai',
-    author_email = 'dhananjai.eee@gmail.com',
+    name="sensor",
+    version="0.0.1",
+    author="Dhananjai",
+    author_email="dhananjai.eee@gmail.com",
     packages = find_packages(),
-    install_requires = get_requirements()
+    install_requires=get_requirements(),
 )
-
